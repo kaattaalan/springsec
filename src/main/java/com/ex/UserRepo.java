17a7446ebefa5eb12148ccca034005dcd774ad93
Appu <kaattaalan13@gmail.com>
2017-01-05 15:18:34 +0530

    ample commit

--- src/main/java/com/ex/UserRepo.java
+++ src/main/java/com/ex/UserRepo.java
@@ -8,3 +8,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserRepo extends JpaRepository<User,String> {
     User findOne(String username);
 }
+sdeflwefiUWASEFRWAREGOWD0dewdwyuytd7qwdqpwddhdcuwdf
